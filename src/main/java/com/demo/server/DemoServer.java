8f2173a7a71fffbd57a18152da9ae2b7b692bdab
yuyenews <631941527@qq.com>
2021-04-18 23:48:57 +0800

    升级依赖到1.1.5

--- src/main/java/com/demo/server/DemoServer.java
+++ src/main/java/com/demo/server/DemoServer.java
@@ -10,7 +10,7 @@ public class DemoServer {
 
             Magician.createHttpServer()
                     .bind(8080, 100)// 设置端口号和最大连接数
-                    .threadPool(DemoThreadPool.getThreadPoolExecutor())// 设置一个线程池
+                    .threadSize(5) // 5个线程同时处理请求
                     .httpHandler("/", req -> {
 
                         // 在http的handler里面调用web组件
